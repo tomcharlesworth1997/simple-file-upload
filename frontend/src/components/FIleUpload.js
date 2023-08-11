f13b03cf3d1d3951d260449294df34428991bde5
Tom C <tom.charlesworth1997@gmail.com>
2023-08-11 16:47:45 +0100

    Display file content and metadata

--- frontend/src/components/FIleUpload.js
+++ frontend/src/components/FIleUpload.js
@@ -2,14 +2,24 @@ import React, { useState } from 'react';
 
 const FileUpload = () => {
   const [selectedFile, setSelectedFile] = useState(null);
+  const [fileContent, setFileContent] = useState(null);
 
   const handleFileChange = (event) => {
-    setSelectedFile(event.target.files[0]);
+    const file = event.target.files[0];
+    setSelectedFile(file);
+
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const fileContents = e.target.result;
+      setFileContent(fileContents);
+    };
+    reader.readAsText(file); // You can use other methods like readAsDataURL or readAsArrayBuffer for different use cases
   };
 
   const handleUpload = () => {
     if (selectedFile) {
-      console.log('Uploading file:', selectedFile.name);
+        console.log(selectedFile)
+        console.log(typeof selectedFile)
     }
   };
 
@@ -18,7 +28,21 @@ const FileUpload = () => {
       <h2>File Upload</h2>
       <input type="file" onChange={handleFileChange} />
       <button onClick={handleUpload}>Upload</button>
-    </div>
+      {selectedFile && (
+        <div>
+          <p>Selected File: {selectedFile.name}</p>
+          <p>File Size: {selectedFile.size} bytes</p>
+          <p>File Type: {selectedFile.type}</p>
+        </div>
+      )}
+
+      {fileContent && (
+        <div>
+            <p>File Content: {fileContent}</p>
+        </div>
+      )}
+    </
+    div>
   );
 };
 
